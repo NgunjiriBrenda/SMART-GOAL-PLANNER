6dd6aa520473ab14e0cfde9d1ebe9ab080e6b065
brenda njoki <ngunjiribrenda5@gmail.com>
2025-07-21 13:00:41 +0300

    Updates Edit function

--- Smart-Goal-Planner/src/App.jsx
+++ Smart-Goal-Planner/src/App.jsx
@@ -8,7 +8,6 @@ function App(){
   const[category, setCategory] = useState("");
   const[deadline, setDeadline] = useState("");
   const[createdAt, setCreatedAt] = useState("");
-  const[body, setBody] = useState("")
   const[editingGoal, setEditingGoal]= useState("")
 
   useEffect(() =>{
@@ -17,17 +16,43 @@ function App(){
     .then((data) =>setGoals(data))
   },[]);
 
+  function resetForm(){
+    setName("");
+    setTargetAmount("");
+    setSavedAmount("");
+    setCategory("");
+    setDeadline("");
+    setCreatedAt("");
+    setEditingGoal("");
+  }
+
   function handleSubmit(e){
     e.preventDefault();
-    const newGoal={name,targetAmount,savedAmount,category,deadline,createdAt,body};
+    if(editingGoal){
+     const newGoal={name,targetAmount,savedAmount,category,deadline,createdAt,body};
+
+    fetch(`http://localhost:3001/goals/${editingGoal.id}`,{
+      method: "PUT",
+      headers: {"Content-Type": "application/json",
+
+      },
+      body: JSON.stringify({
+        name,targetAmount,savedAmount,category, deadline,
+      })
+    })
+    .then((res) => res.json())
+    .then(updatedGoal => {
+      setGoals(goals.map(goal => (goal.id === updatedGoal.id ?updatedGoal: goal)));
+      resetForm();
+    });
+  }else {;
+     const newGoal={name,targetAmount,savedAmount,category,deadline,createdAt};
 
     fetch("http://localhost:3001/goals",{
       method: "POST",
       headers: {"Content-Type": "application/json"
-
       },
       body: JSON.stringify(newGoal)
-
     })
     .then((res) => res.json())
     .then((data) =>{
@@ -38,9 +63,9 @@ function App(){
       setCategory("");
       setDeadline("");
       setCreatedAt("");
-      setBody("");
     });
   }
+}
 
   function handleDelete(id){
     fetch(`http://localhost:3001/goals/${id}`,{
@@ -50,7 +75,7 @@ function App(){
       const updateGoals = goals.filter(goal =>goal.id !== id);
       setGoals(updateGoals);
   })
-
+}
   function handleEdit(goal){
     setEditingGoal(goal);
     setName(goal.name);
@@ -58,17 +83,13 @@ function App(){
     setSavedAmount(goal.savedAmount);
     setCategory(goal.category);
     setDeadline(goal.deadline);
+    setCreatedAt(goal.createdAt);
   }
   
-
-
-  }
-    
   return (
     <div>
       <h1>Smart Goal Planner</h1>
 
-      {/* Display goals */}
       {goals.map((goal) => (
         <div key={goal.id}>
           <strong>{goal.name}</strong>
@@ -77,60 +98,23 @@ function App(){
           <div>Category: {goal.category}</div>
           <div>Deadline: {goal.deadline}</div>
           <div>Created: {goal.createdAt}</div>
-          <p>{goal.body}</p>
           <button onClick={() => handleEdit(goal)}>Edit</button>
           <button onClick={() => handleDelete(goal.id)}>Delete</button>
         </div>
       ))}
 
-      {/* Add new goal form */}
       <form onSubmit={handleSubmit}>
         <h2>Add a new Goal</h2>
-        <input
-          type="text"
-          placeholder="Goal name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <textarea
-          placeholder="Goal Body"
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-        ></textarea>
-        <input
-          type="number"
-          placeholder="Target amount"
-          value={targetAmount}
-          onChange={(e) => setTargetAmount(e.target.value)}
-        />
-        <input
-          type="number"
-          placeholder="Saved amount"
-          value={savedAmount}
-          onChange={(e) => setSavedAmount(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        />
-        <input
-          type="date"
-          placeholder="Deadline"
-          value={deadline}
-          onChange={(e) => setDeadline(e.target.value)}
-        />
-        <input
-          type="date"
-          placeholder="Created"
-          value={createdAt}
-          onChange={(e) => setCreatedAt(e.target.value)}
-        />
+        <input type="text"placeholder="Goal name"value={name}onChange={(e) => setName(e.target.value)}/>
+        <input type="number"placeholder="Target amount"value={targetAmount}onChange={(e) => setTargetAmount(e.target.value)}/>
+        <input type="number"placeholder="Saved amount"value={savedAmount}onChange={(e) => setSavedAmount(e.target.value)}/>
+        <input type="text"placeholder="Category"value={category}onChange={(e) => setCategory(e.target.value)}/>
+        <input type="date"placeholder="Deadline"value={deadline}onChange={(e) => setDeadline(e.target.value)}/>
+        <input type="date"placeholder="Created"value={createdAt}onChange={(e) => setCreatedAt(e.target.value)}/>
         <button type="submit">Submit</button>
       </form>
     </div>
-  );
-}
+     );
+}    
 
 export default App;
